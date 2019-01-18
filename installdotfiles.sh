6812c4d78c2f855e45be1d1bcc7f29b782b10f9f
codeChG <46350972+codeChG@users.noreply.github.com>
2019-01-18 17:46:36 +0100

    header added

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+###########################################################
+#							                                            #
+#  clont bare repo von github u.installiert dotfiles im ~ #
+#							                                            #
+###########################################################
+
 sudo pacman -S git curl --noconfirm --needed
 git clone --bare https://github.com/codeChG/dotFiles $HOME/.cfg
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
