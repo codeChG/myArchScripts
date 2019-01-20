ffe2dd774d077ccd73a3f916dbd3acec9449fc04
codeChG <46350972+codeChG@users.noreply.github.com>
2019-01-20 12:17:53 +0100

    cat mirrorlist added

--- changePacmanMirrorlist.sh
+++ changePacmanMirrorlist.sh
@@ -10,8 +10,8 @@ echo '<cg> getting the fastest mirrors and saves them in new mirrorlist'
 
 sudo pacman -S --noconfirm --needed pacman-contrib reflector
 
-sudo reflector  --verbose --country germany --sort rate --protocol https \
---latest 30 --number 10 --save /etc/pacman.d/mirrorlist
+sudo reflector  --verbose --country germany --sort rate --protocol https --latest 30 \
+--number 10 --save /etc/pacman.d/mirrorlist && cat /etc/pacman.d/mirrorlist 
 
 
 
