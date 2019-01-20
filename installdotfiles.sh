8d6cbdac3d7cac40623c5fec7205d2d9652dd510
codeChG <46350972+codeChG@users.noreply.github.com>
2019-01-20 19:21:36 +0100

    $CONFIG

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -4,10 +4,14 @@
 #  clont bare repo von github u.installiert dotfiles im ~ #
 ###########################################################
 
+CONFIG="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+
 sudo pacman -S git curl --noconfirm --needed
 git clone --bare https://github.com/codeChG/dotFiles $HOME/.cfg
-alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-config config --local status.showUntrackedFiles no
-config checkout arch
-config status
-ls -al
+#alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+$CONFIG config --local status.showUntrackedFiles no
+
+$CONFIG checkout arch
+$CONFIG status
+
