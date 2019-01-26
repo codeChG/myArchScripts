998252fc8a6abd27c8d2b6cb2ec8cd03bbe9bb5d
Christian Grimm <ch.grimm@gmx.net>
2019-01-26 19:06:21 +0100

    more files...

---
+++ msfonts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+USERFONTDIR="$HOME/.local/share/fonts/TTF"
+
+
+if ! [[ -x $(which git) ]]; then
+  pacman -Sy git
+fi
+
+cd /tmp
+git clone https://github.com/codeChG/msfonts.git
+echo "----------------------------------"
+echo "copying msfonts to $USERFONTDIR..."
+echo "----------------------------------"
+mkdir -p $USERFONTDIR
+cp /tmp/msfonts/* $USERFONTDIR -rf
+echo "----------------------------------"
+echo "updating all caches..."
+echo "----------------------------------"
+cd $USERFONTDIR
+fc-cache -vf
+mkfontscale
+mkfontdir
+# cleaning!
+rm -rf /tmp/msfonts
+echo "----------------------------------"
+echo "done."
+echo "----------------------------------"
+
---
+++ setscreenres.sh
@@ -0,0 +1,1 @@
+xrandr --output VGA-1 --mode 1600x900
