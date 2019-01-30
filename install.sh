3111fc3e8b8be59351cca2e912a21717c8439a1e
Christian Grimm <ch.grimm@gmx.net>
2019-01-30 20:54:49 +0100

    bla

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+set -e
 ###########################################################
-#							                                            #
+#				                          #
 #  install                                                #
-#							                                            #
+#				                          #
 ###########################################################
 
 
-. ./changePacmanMirrorlist.sh
-sudo pacman -Syu --noconfirm
+./changePacmanMirrorlist.sh
 . ./installdotfiles.sh
+sudo pacman -Syu --noconfirm
+./030-install-xorg-virtualbox.sh
+./msfonts.sh
+./germankeyboard
