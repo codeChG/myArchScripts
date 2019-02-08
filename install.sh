a3d1bcfd094325c65e906aa5be179b1a83084a3c
Christian Grimm <ch.grimm@gmx.net>
2019-02-08 19:47:52 +0100

    comment edited

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-set -e
-###########################################################
+#set -e
+###########################################
 #				                          #
-#  install                                                #
+#  install                                #
 #				                          #
-###########################################################
+###########################################
 
 echo cg installing ...
 ./changePacmanMirrorlist.sh
 . ./installdotfiles.sh
 . $HOME/.bash_aliases
 sudo pacman -Syu --noconfirm
-./030-install-xorg-virtualbox.sh
+./030-install-xorg-intel.sh
 ./msfonts.sh
 ./germankeyboard
-./040-install-i3-gaps.sha
+./040-install-i3-gaps.sh
 echo "################################"
 echo "done - reboot this maschine now!"
 echo "################################"
