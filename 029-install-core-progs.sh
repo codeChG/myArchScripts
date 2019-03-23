cbf21c420a2b9ac8c42c872ff654b25c5cd82314
Christian Grimm <ch.grimm@gmx.net>
2019-03-23 18:02:45 +0100

    pcalc 2

--- 029-install-core-progs.sh
+++ 029-install-core-progs.sh
@@ -9,12 +9,10 @@ set -e
 
 echo "029 install core programs <<cg>>"
 
-sudo pacman -S --noconfirm --needed gdb zip unzip 
-
+sudo pacman -S --noconfirm --needed zip unzip 
 sudo pacman -S --noconfirm --needed feh figlet fish hexedit nodejs
 
 [ ! -f /usr/bin/ufetch ] && packer -S --noconfirm ufetch-git
-
-packer -S --noconfirm pcalc
+[ ! -f /usr/bin/ufetch ] && packer -S --noconfirm pcalc
 
 echo "029 install core programs <<cg>> done."
