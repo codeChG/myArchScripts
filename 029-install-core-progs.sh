57e005ed1d0f86cd1b890e336a9799a929e4859f
Christian Grimm <ch.grimm@gmx.net>
2019-03-31 19:47:34 +0200

    gnu-netcat

--- 029-install-core-progs.sh
+++ 029-install-core-progs.sh
@@ -12,6 +12,8 @@ echo "029 install core programs <<cg>>"
 sudo pacman -S --noconfirm --needed zip unzip 
 sudo pacman -S --noconfirm --needed feh figlet fish hexedit nodejs
 
+sudo pacman -S --noconfirm --needed gnu-netcat
+
 [ ! -f /usr/bin/ufetch ] && packer -S --noconfirm ufetch-git
 [ ! -f /usr/bin/ufetch ] && packer -S --noconfirm pcalc
 
